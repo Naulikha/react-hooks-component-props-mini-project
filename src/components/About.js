1b8f1939c4df3f020ec9c8d8c6d2fda4fbc632b7
Naulikha <nbiketia@gmail.com>
2023-04-23 18:56:22 +0300

    Completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About({
+image = "https://via.placeholder.com/215",
+aboutText
+}) 
+{
+    return (
+      <>
+      <aside>
+        <img src={image} alt="blog logo"></img>
+        <p>{aboutText}</p>
+      </aside>
+      </>
+    );
+  }
+  
+  export default About;
+  
